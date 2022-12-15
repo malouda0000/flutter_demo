f050eaa1ab3b99a55100b9b68800f90a8bc41810
malouda0000 <hamam.hamza85@gmail.com>
2022-12-15 19:36:53 +0200

    again, its refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/core/constants/app_colors.dart';
 import 'package:flutter_demo/core/view/screens/home/home_screen.dart';
-import 'package:flutter_demo/core/view/screens/singin_screen.dart';
+import 'package:flutter_demo/core/view/screens/signin/singin_screen.dart';
 import 'package:get/get.dart';
 
 void main() {
