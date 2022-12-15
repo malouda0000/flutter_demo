1e33ba29a09612bc4c35ba880e3bf67eac68f7a2
malouda0000 <hamam.hamza85@gmail.com>
2022-12-15 13:01:11 +0200

    solved small prob in the home screen

--- lib/core/view/screens/singin_screen.dart
+++ lib/core/view/screens/singin_screen.dart
@@ -26,143 +26,176 @@ class SingInScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(
             horizontal: theDefaultPadding,
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              const SizedBox(
-                height: 84,
-              ),
-              Center(
-                child: SvgPicture.asset(
-                  // i have used svg images for better qualty
-                  AppImages.mainLogoSvg,
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const SizedBox(
+                  height: 84,
                 ),
-              ),
-              const SizedBox(
-                height: 42,
-              ),
-              // TitleBuilder(
-              //   theTitle: 'الاسم',
-              //   isBold: true,
-              // ),
+                Center(
+                  child: SvgPicture.asset(
+                    // i have used svg images for better qualty
+                    AppImages.mainLogoSvg,
+                  ),
+                ),
+                const SizedBox(
+                  height: 42,
+                ),
+                // TitleBuilder(
+                //   theTitle: 'الاسم',
+                //   isBold: true,
+                // ),
 
-              Text(
-                "الاسم",
-                style: Theme.of(context).textTheme.headline6!.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              emptySpace,
-              TheInputField(
-                // user name
-                theHient: 'محمد احمد صالح سعيد',
-                theEditingController: userNaEditingController!,
-                isPassword: false,
-                theBorderColor: AppColor.kPrimaryColor,
-                theInputType: TextInputType.text,
-                theRadus: theDefaultRaduis,
-              ),
-              emptySpace,
-              emptySpace,
-              Text(
-                "رقم الهاتف",
-                style: Theme.of(context).textTheme.headline6!.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              emptySpace,
-              TheInputField(
-                // phone num
-                theHient: 'محمد احمد صالح سعيد',
-                theEditingController: phoneNumEditingController!,
-
-                isPassword: false,
-                theBorderColor: AppColor.kPrimaryColor,
-                theInputType: TextInputType.text,
-                theRadus: theDefaultRaduis,
-              ),
-              emptySpace,
-              emptySpace,
-              Text(
-                "كلمة السر",
-                style: Theme.of(context).textTheme.headline6!.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              emptySpace,
-              TheInputField(
-                // password
-                theHient: 'كلمة السر',
-                theEditingController: passwordEditingController!,
-
-                isPassword: false,
-                theBorderColor: AppColor.kPrimaryColor,
-                theInputType: TextInputType.text,
-                theRadus: theDefaultRaduis,
-              ),
-              emptySpace,
-              emptySpace,
-              TheInputField(
-                // repassword
-                theHient: 'تاكيد كلمة السر',
-                theEditingController: repasswordEditingController!,
-
-                isPassword: false,
-                theInputType: TextInputType.text,
-                theBorderColor: AppColor.kPrimaryColor,
-                theRadus: theDefaultRaduis,
-              ),
-              SizedBox(
-                height: 42,
-              ),
-
-              BigggButton(
-                theButtonTitle: 'انشاء حساب',
-                onTaped: () {
-                  Get.to(HomeScreen());
-                },
-                theTextColor: Colors.white,
-                theButtonColor: AppColor.kPrimaryColor,
-              ),
-
-              emptySpace,
-              emptySpace,
-              Center(
-                child: Text(
-                  "لديك حساب مسبقا؟",
+                Text(
+                  "الاسم",
                   style: Theme.of(context).textTheme.headline6!.copyWith(
-                        color: AppColor.kGreyTextColor,
+                        fontWeight: FontWeight.bold,
                       ),
-                  textAlign: TextAlign.center,
                 ),
-              ),
-
-              emptySpace,
-              emptySpace,
-
-              Container(
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(
-                    theSmallPadding,
-                  ),
-                  border: Border.all(
-                    color: AppColor.kPrimaryColor,
-                  ),
+                emptySpace,
+                TheInputField(
+                  // user name
+                  theHient: 'محمد احمد صالح سعيد',
+                  theEditingController: userNaEditingController!,
+                  isPassword: false,
+                  theBorderColor: AppColor.kPrimaryColor,
+                  theInputType: TextInputType.text,
+                  theRadus: theDefaultRaduis,
                 ),
-                child: BigggButton(
-                  theButtonTitle: 'تسجيل دخول',
+                emptySpace,
+                emptySpace,
+                Text(
+                  "رقم الهاتف",
+                  style: Theme.of(context).textTheme.headline6!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                emptySpace,
+                TheInputField(
+                  // phone num
+                  theHient: 'محمد احمد صالح سعيد',
+                  theEditingController: phoneNumEditingController!,
+
+                  isPassword: false,
+                  theBorderColor: AppColor.kPrimaryColor,
+                  theInputType: TextInputType.text,
+                  theRadus: theDefaultRaduis,
+                ),
+                emptySpace,
+                emptySpace,
+                Text(
+                  "كلمة السر",
+                  style: Theme.of(context).textTheme.headline6!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                emptySpace,
+                TheInputField(
+                  // password
+                  theHient: 'كلمة السر',
+                  theEditingController: passwordEditingController!,
+
+                  isPassword: false,
+                  theBorderColor: AppColor.kPrimaryColor,
+                  theInputType: TextInputType.text,
+                  theRadus: theDefaultRaduis,
+                ),
+                emptySpace,
+                emptySpace,
+                TheInputField(
+                  // repassword
+                  theHient: 'تاكيد كلمة السر',
+                  theEditingController: repasswordEditingController!,
+
+                  isPassword: false,
+                  theInputType: TextInputType.text,
+                  theBorderColor: AppColor.kPrimaryColor,
+                  theRadus: theDefaultRaduis,
+                ),
+                SizedBox(
+                  height: 42,
+                ),
+
+                BigggButton(
+                  theButtonTitle: 'انشاء حساب',
                   onTaped: () {
                     Get.to(HomeScreen());
                   },
-                  theTextColor: AppColor.kPrimaryColor,
-                  theButtonColor: Theme.of(context).scaffoldBackgroundColor,
+                  theTextColor: Colors.white,
+                  theButtonColor: AppColor.kPrimaryColor,
                 ),
-              ),
-            ],
+
+                emptySpace,
+                emptySpace,
+                Center(
+                  child: Text(
+                    "لديك حساب مسبقا؟",
+                    style: Theme.of(context).textTheme.headline6!.copyWith(
+                          color: AppColor.kGreyTextColor,
+                        ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+
+                emptySpace,
+                emptySpace,
+
+                Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      theSmallPadding,
+                    ),
+                    border: Border.all(
+                      color: AppColor.kPrimaryColor,
+                    ),
+                  ),
+                  child: BigggButton(
+                    theButtonTitle: 'تسجيل دخول',
+                    onTaped: () {
+                      Get.to(HomeScreen());
+                    },
+                    theTextColor: AppColor.kPrimaryColor,
+                    theButtonColor: Theme.of(context).scaffoldBackgroundColor,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       )),
     );
   }
 }
+
+// class _customFormFiea extends StatelessWidget {
+//   const _customFormFiea({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return 
+//           Expanded(
+//             child: TextFormField(
+//               textDirection: TextDirection.rtl,
+//               // validator: theValidator,
+//               // onSubmitted: ,
+//               // key: theKey,
+//               controller: serchTextEditingController,
+//               keyboardType: TextInputType.text,
+//               style: Theme.of(context).textTheme.bodyText1,
+//               decoration: InputDecoration(
+//                 // fillColor: AppColor.kPrimaryColor.withOpacity(.2),
+//                 border: InputBorder.none,
+//                 hintText: 'ابحث عن طلبك',
+//                 hintStyle: Theme.of(context).textTheme.bodyText1!.copyWith(
+//                       color: AppColor.kLightTextColor,
+//                     ),
+//                 // hintStyle: TextStyle(
+//                 //   color: Colors.black,
+//                 // ),
+//               ),
+//             ),
+//           );
+//   }
+// }
